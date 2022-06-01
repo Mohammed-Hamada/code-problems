bd7a2483a0ebe056fedaa0e2b49ba4a62142e1df
Mohammed Hamada <71943250+Mohammed-Hamada@users.noreply.github.com>
2022-06-01 06:32:49 +0300

    Time: 105 ms (50.54%), Space: 45.9 MB (60.42%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -5,8 +5,8 @@
 
 const buildArray = (nums) => {
   let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    ans.push(nums[nums[i]]);
-  }
-  return ans;
+  for(let i = 0; i < nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;     
 };
