5120228544c4d09fb0673240368829e50842220a
Mohammed Hamada <71943250+Mohammed-Hamada@users.noreply.github.com>
2022-06-03 03:37:32 +0300

    Time: 127 ms (48.44%), Space: 51 MB (52.20%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,5 @@
  * @return {boolean}
  */
 const containsDuplicate = (nums) => {
-  if (nums.length === 1) return false;
-  else if (nums[0] === nums[1] || nums[0] === nums[nums.length - 1]) return true;
-  nums = nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) {
-      return true;
-    }
-  }
-  return false;
+  return Array.from(new Set(nums)).length < nums.length;
 };
