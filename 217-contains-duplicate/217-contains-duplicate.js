3eba604aa85394e37335d212b9194fb4ed6c4187
Mohammed Hamada <71943250+Mohammed-Hamada@users.noreply.github.com>
2022-06-03 03:51:40 +0300

    Time: 140 ms (39.35%), Space: 50.9 MB (59.19%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,20 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const containsDuplicate = (nums) => {
-  let obj = {};
-  for (let i = 0; i < nums.length; ++i) {
-    if (obj[nums[i]]) return true;
-    else{
-      obj[nums[i]] = true
-    }
-  }
-  return false
-};
+// const containsDuplicate = (nums) => {
+//   let obj = {};
+//   for (let i = 0; i < nums.length; ++i) {
+//     if (obj[nums[i]]) return true;
+//     else{
+//       obj[nums[i]] = true
+//     }
+//   }
+//   return false
+// };
 
 // Another Solution
-/*
+
 const containsDuplicate = (nums) => {
   return new Set(nums).size !== nums.length;
 };
-*/
