cc87da2b10c81a548800447ab7c6ae9f87f8e740
Mohammed Hamada <71943250+Mohammed-Hamada@users.noreply.github.com>
2023-09-19 00:39:43 +0300

    Time: 77 ms (60.04%), Space: 50.6 MB (21.17%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,40 +12,21 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function (headA, headB) {
-    let l1 = 0;
-  let l2 = 0;
+    const nodes = new Set();
 
-  let curr1 = headA;
-  let curr2 = headB;
-
-  while (curr1 !== null) {
-    curr1 = curr1.next;
-    l1++;
-  }
-  while (curr2 !== null) {
-    curr2 = curr2.next;
-    l2++;
-  }
-
-  const d = Math.abs(l1 - l2);
-
-  curr1 = headA;
-  curr2 = headB;
-
-  if (l1 > l2) {
-    for (let i = 0; i < d; i++) {
-      curr1 = curr1.next;
+    let curr1 = headA;
+    while (curr1 !== null) {
+        nodes.add(curr1);
+        curr1 = curr1.next;
     }
-  } else {
-    for (let i = 0; i < d; i++) {
-      curr2 = curr2.next;
+
+    let curr2 = headB;
+    while (curr2 !== null) {
+        if (nodes.has(curr2)) {
+            return curr2;
+        }
+        curr2 = curr2.next;
     }
-  }
 
-  while (curr1 !== curr2) {
-    curr1 = curr1.next;
-    curr2 = curr2.next;
-  }
-
-  return curr1;
+    return null;
 };
